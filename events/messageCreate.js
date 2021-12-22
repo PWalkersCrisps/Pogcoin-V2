f1c0b4942028dec8a63227b28277340d3fe010f7
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-12-22 09:10:03 +0000

    Added Events

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,25 +1,56 @@
+const { MessageEmbed } = require('discord.js');
 const { currentDate } = require('../modules/globalVariables');
 const { addPogcoin } = require('../modules/modifyPogcoin.js');
 const profileCreate = require('../modules/profileCreate.js');
 const coinCooldown = new Set();
 
-
+const cooldownModel = require('../models/cooldownSchema.js');
 const profileModel = require('../models/profileSchema.js');
+const statsModel = require('../models/statsSchema.js');
 
 module.exports = {
 	name: 'messageCreate',
 	async execute(message) {
         try {
+            if (message.author.bot) return; // Checks if the user talking is a bot or not, if so, it ignores the message
 
-            const profileData = profileModel.findOne({ userID: message.author.id });
+            const profileData = await profileModel.findOne({ userID: message.author.id }); // If they somehow didnt get a profile created when they joined, this here will create them a new profile
+            const cooldownData = await cooldownModel.findOne({ userID: message.author.id }); // If they somehow didnt get a profile created when they joined, this here will create them a new profile
+            const statsData = await statsModel.findOne({ userID: message.author.id }); // If they somehow didnt get a profile created when they joined, this here will create them a new profile
+            if (!cooldownData) {
+                profileCreate.createUserProfile(message.author.id, 2);
+            }
             if (!profileData) {
-                profileCreate.createUserProfile(message.author.id);
+                profileCreate.createUserProfile(message.author.id, 3);
+            }
+            if (!statsData) {
+                profileCreate.createUserProfile(message.author.id, 4);
             }
 
-            if (Math.random() < 0.005 && !coinCooldown.has(message.author.id)) {
-                addPogcoin(message.author.id, 1);
+            if (Math.random() < 0.50 && !coinCooldown.has(message.author.id)) {
+                addPogcoin(message.author.id, 1, true); // Adds 1 pogcoin to the user while also changing their stats
+                let genderRole;
+                if (message.member.roles.cache.some(role => role.name === 'He/Him')) { // checks if the auther has the he/him role
+                    genderRole = 'boy';
+                }
+                else if (message.member.roles.cache.some(role => role.name === 'She/Her')) { // checks if the auther has the she/her role
+                    genderRole = 'girl';
+                }
+                else { // If the user has the they/them or dont have a gender role, it will always default to this
+                    genderRole = 'child';
+                }
+
+                const pogcoinEarnt = new MessageEmbed()
+                .addFields(
+                    { name: 'You got a pogcoin!', value: `Amazing, im proud of you, you are a good ${genderRole}` },
+                )
+                .setTimestamp()
+                .setFooter('This is mininum wage noob');
+
+                message.author.send({ embeds: [pogcoinEarnt] });
+
                 coinCooldown.add(message.author.id);
-                setTimeout(() => { coinCooldown.delete(message.author.id); }, 1 * 60000);
+                setTimeout(() => { coinCooldown.delete(message.author.id); }, 1 * 1000);
             }
         }
         catch (error) {
