9f9878095bf51b585f4a52d9c2a7877ed3059be6
PWalkersCrisps <pwalkerscrisps@gmail.com>
2021-12-28 12:25:00 +0000

    Prepping commit to main

--- arrays/interactionCommands.js
+++ arrays/interactionCommands.js
@@ -11,69 +11,8 @@ module.exports = [
     .addUserOption(option => option.setName('target').setDescription('Select a user')),
 
     new SlashCommandBuilder()
-    .setName('create')
-    .setDescription('Creates something i guess')
-    .addSubcommand(subcommand =>
-		subcommand
-			.setName('profile')
-			.setDescription('Create a user profile in case of debugging')
-			.addUserOption(option => option.setName('target').setDescription('Select a user').setRequired(true))),
-
-    new SlashCommandBuilder()
-    .setName('modify')
-    .setDescription('Creates something i guess')
-    .addSubcommand(subcommand =>
-        subcommand
-            .setName('give')
-            .setDescription('Give a user some coins')
-            .addUserOption(option => option.setName('target').setDescription('Select a user').setRequired(true))
-            .addIntegerOption(option => option.setName('int').setDescription('Enter an integer').setRequired(true))
-            .addBooleanOption(option => option.setName('choice').setDescription('Do you want this to modify their profile stats?').setRequired(true)))
-    .addSubcommand(subcommand =>
-        subcommand
-            .setName('remove')
-            .setDescription('remove some coins from a user')
-            .addUserOption(option => option.setName('target').setDescription('Select a user').setRequired(true))
-            .addIntegerOption(option => option.setName('int').setDescription('Enter an integer').setRequired(true))
-            .addBooleanOption(option => option.setName('choice').setDescription('Do you want this to modify their profile stats?').setRequired(true)))
-    .addSubcommand(subcommand =>
-        subcommand
-            .setName('reset')
-            .setDescription('reset a user\'s coins as well as their stats')
-            .addUserOption(option => option.setName('target').setDescription('Select a user').setRequired(true))),
-
-    new SlashCommandBuilder()
-    .setName('shop')
-    .setDescription('Check out what the shop is like')
-    .addSubcommand(subcommand =>
-        subcommand
-            .setName('buy')
-            .setDescription('Buy something from the shop')
-            .addStringOption(option => option.setName('input').setDescription('Item you want to buy (Case insenstive)').setRequired(true)))
-    .addSubcommand(subcommand =>
-        subcommand
-            .setName('list')
-            .setDescription('See what items are in the shop')),
-
-    new SlashCommandBuilder()
-    .setName('profile')
-    .setDescription('Replies with yours or someone elses profile')
-    .addUserOption(option => option.setName('target').setDescription('Select a user')),
-
-    new SlashCommandBuilder()
-    .setName('slots')
-    .setDescription('Gamble all of your life savings away')
-    .addIntegerOption(option => option.setName('amount').setDescription('How much do you want to gamble?')),
-
-    new SlashCommandBuilder()
-    .setName('ltgcheck')
-    .setDescription('Get a quick check up'),
-
-    new SlashCommandBuilder()
-    .setName('nya')
-    .setDescription('UwU heheheha'),
-
-    new SlashCommandBuilder()
-    .setName('help')
-    .setDescription('Helps you with shit you didnt know'),
+    .setName('donate')
+    .setDescription('Give someone your hard earnt money')
+    .addUserOption(option => option.setName('target').setDescription('Select a user'))
+    .addIntegerOption(option => option.setName('amount').setDescription('How much do you want to donate?')),
 ];
