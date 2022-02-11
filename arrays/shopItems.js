024ce106cfbef8082f9fa3d55323ad1ba50a5598
PWalkersCrisps <pwalkerscrisps@gmail.com>
2022-02-11 10:14:01 +0000

    Update shopItems.js

--- arrays/shopItems.js
+++ arrays/shopItems.js
@@ -1,3 +1,6 @@
 module.exports = [
     { itemName: 'Image Perms', itemPrice: 25, mode: 1, modeItem: '914792018130137108' },
+    { itemName: 'Mario Role', itemPrice: 5, mode: 1, modeItem: '938122435876782132' },
+    { itemName: 'Luigi Role', itemPrice: 5, mode: 1, modeItem: '938123439271714916' },
+    { itemName: 'Wario Role', itemPrice: 5, mode: 1, modeItem: '938123628371914792' },
 ];
