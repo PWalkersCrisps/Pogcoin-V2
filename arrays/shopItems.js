a5a394f7f87cb662998c3a1db190b8e93c7061a5
PWalkersCrisps <pwalkerscrisps@gmail.com>
2022-02-13 08:53:22 +0000

    Update shopItems.js

--- arrays/shopItems.js
+++ arrays/shopItems.js
@@ -1,6 +1,13 @@
+/*
+    Mode 1 = give role
+    Mode 2 = DM the owner
+*/
 module.exports = [
     { itemName: 'Image Perms', itemPrice: 25, mode: 1, modeItem: '914792018130137108' },
     { itemName: 'Mario Role', itemPrice: 5, mode: 1, modeItem: '938122435876782132' },
     { itemName: 'Luigi Role', itemPrice: 5, mode: 1, modeItem: '938123439271714916' },
     { itemName: 'Wario Role', itemPrice: 5, mode: 1, modeItem: '938123628371914792' },
+    { itemName: 'Add an emote', itemPrice: 100, mode: 2, modeItem: 'has just brought "Add an emote", ig ask them what they want to add' },
+    { itemName: 'Add a sticker', itemPrice: 500, mode: 2, modeItem: 'has just brought "Add an sticker", ig ask them what they want to add' },
+    { itemName: 'Staff Furry Chat Role', itemPrice: 1000, mode: 1, modeItem: '919982459230236722' },
 ];
