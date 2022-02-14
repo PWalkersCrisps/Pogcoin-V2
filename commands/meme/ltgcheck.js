fb2a7eba8f47f63077102de8c412160b02296bea
PWalkersCrisps <pwalkerscrisps@gmail.com>
2022-02-14 21:40:03 +0000

    Update ltgcheck.js

--- commands/meme/ltgcheck.js
+++ commands/meme/ltgcheck.js
@@ -1,14 +1,20 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { ltgCheck } = require('../../arrays/randomResponses.js');
 const random = require('../../modules/random.js');
+const weightedRandom = require('weighted-random');
 module.exports = {
     name: 'ltgcheck',
     description: 'Lose ALL of your money!',
     data: new SlashCommandBuilder().setName('ltgcheck')
     .setDescription('Get a quick check up'),
     async execute(client, interaction, MessageEmbed, MessageActionRow, MessageButton, profileData) {
+
+        const weights = ltgCheck.map(function(check) {
+            return check.weight;
+        });
+
         const ltgEmbed = new MessageEmbed()
-        .setImage(ltgCheck[Math.floor(Math.random() * ltgCheck.length)])
+        .setImage(ltgCheck[weightedRandom(weights)].image)
         .setColor(random.randomHexColour())
         .setTitle('Low Tier God\'s advice');
 
