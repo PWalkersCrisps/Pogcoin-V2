2e9671ff3f38605b58d73edc55161170780e0451
PWalkersCrisps <pwalkerscrisps@gmail.com>
2022-02-15 19:38:52 +0000

    Update nya.js

--- commands/meme/nya.js
+++ commands/meme/nya.js
@@ -23,6 +23,7 @@ module.exports = {
 
             await interaction.reply({ embeds: [nyaEmbed] });
 
+            nyaCooldown.add(interaction.guild.id);
             setTimeout(() => { nyaCooldown.delete(interaction.guild.id); }, 300000);
         }
         else {
