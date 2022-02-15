3510d4d4530675c64e6f41af5394c2936a96790e
PWalkersCrisps <pwalkerscrisps@gmail.com>
2022-02-15 20:43:39 +0000

    Update create.js

--- commands/debug/create.js
+++ commands/debug/create.js
@@ -10,14 +10,27 @@ module.exports = {
 		subcommand
 			.setName('profile')
 			.setDescription('Create a user profile in case of debugging')
-			.addUserOption(option => option.setName('target').setDescription('Select a user').setRequired(true))),
+			.addUserOption(option => option.setName('target').setDescription('Select a user').setRequired(true)))
+    .addSubcommand(subcommand =>
+        subcommand
+            .setName('server')
+            .setDescription('Create a server just for shits and giggles')),
     async execute(client, interaction, MessageEmbed, MessageActionRow, MessageButton, profileData) {
         if (!userPerms.includes(interaction.user.id) || interaction.user.id == '426455031571677197') return interaction.reply({ content: `<@${interaction.user.id}> actually have permissions to use the command next time`, ephemeral: true });
+        let newGuild;
         const userMentioned = interaction.options.getMember('target');
         switch (interaction.options.getSubcommand()) {
             case 'profile':
                 profileCreate.createUserProfile(userMentioned.id); // Cteates a profile for the user that was mentioned
                 interaction.reply({ content: `User profile created for <@${userMentioned.id}>`, ephemeral: true });
+                break;
+            case 'server':
+                if (!interaction.user.id == '426455031571677197') return interaction.reply({ content: `<@${interaction.user.id}> actually have permissions to use the command next time`, ephemeral: true });
+                newGuild = await client.guilds.create(`Pogcoin\'s server ${Date().toLocaleTimeString()}`, {
+                    channels: [
+                        { 'name': 'general' },
+                    ],
+                });
                 break;
         }
     },
