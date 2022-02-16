475d1398714d5b1d0307aa9ed6fd1e7f5b3fe241
PWalkersCrisps <pwalkerscrisps@gmail.com>
2022-02-16 18:37:25 +0000

    Update echo.js

--- commands/debug/echo.js
+++ commands/debug/echo.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const userPerms = require('../../arrays/userPerms.js');
+/*
 module.exports = {
     name: 'echo',
     description: 'Echo your message',
@@ -7,7 +8,7 @@ module.exports = {
     .setDescription('Echo your message')
     .addStringOption(option => option.setName('input').setDescription('Enter a string')),
     async execute(client, interaction, MessageEmbed, MessageActionRow, MessageButton, profileData) {
-        const mods = [
+        const perms = [
             '625052755471630346',
             '880171403977064528',
             '322422240836583435',
@@ -18,12 +19,15 @@ module.exports = {
             '301127339599986689',
             '426455031571677197',
         ];
-        if (!mods.includes(interaction.user.id)) return interaction.reply({ content: `<@${interaction.user.id}> actually have permissions to use the command next time`, ephemeral: true });
+        if (!perms.includes(interaction.user.id)) return interaction.reply({ content: `<@${interaction.user.id}> actually have permissions to use the command next time`, ephemeral: true });
 
         const string = interaction.options.getString('input');
+
+        console.log(`${interaction.user.username}`)
 
         await interaction.reply({ content: 'Message sent to the idiots', ephemeral: true });
         await interaction.channel.send(`${string}`);
 
     },
 };
+*/
