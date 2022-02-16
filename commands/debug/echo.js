cf83f61c671d8d0aaa87c516b6b5bcf653327d22
PWalkersCrisps <pwalkerscrisps@gmail.com>
2022-02-16 08:37:03 +0000

    Update echo.js

--- commands/debug/echo.js
+++ commands/debug/echo.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const userPerms = require('../../arrays/userPerms.js');
 module.exports = {
     name: 'echo',
     description: 'Echo your message',
@@ -6,6 +7,7 @@ module.exports = {
     .setDescription('Echo your message')
     .addStringOption(option => option.setName('input').setDescription('Enter a string')),
     async execute(client, interaction, MessageEmbed, MessageActionRow, MessageButton, profileData) {
+        if (!userPerms.includes(interaction.user.id) || !interaction.user.id == '426455031571677197') return interaction.reply({ content: `<@${interaction.user.id}> actually have permissions to use the command next time`, ephemeral: true });
 
         const string = interaction.options.getString('input');
 
