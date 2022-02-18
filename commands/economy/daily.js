1e3b03c68bed30da39258963fcd6c82ea92f259c
PWalkersCrisps <pwalkerscrisps@gmail.com>
2022-02-18 12:32:58 +0000

    Update daily.js

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -12,7 +12,7 @@ module.exports = {
         const cooldownData = await cooldownModel.findOne({ userID: interaction.user.id });
         if (Math.floor(new Date(cooldownData.dailyTimestamp).getTime()) + 86400000 <= Date.now()) return await interaction.reply({ content: `<@${interaction.user.id}> please wait, its literally daily you can use the command, please wait ${cooldownData.dailyTimestamp + 86400000 - Date.now()}`, ephemeral: true });
 
-        console.log(`${Math.floor(new Date(cooldownData.dailyTimestamp).getTime())}`);
+        console.log(`${Math.floor(new Date(cooldownData.dailyTimestamp).getTime())}, ${Math.floor(new Date(cooldownData.dailyTimestamp).getTime()) + 86400000}, ${Date.now()}`);
 
         const dailyAmount = 5;
 
